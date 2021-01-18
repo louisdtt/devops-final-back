673937738989b0e78722ce3b85d0abe4836bca55
Louis Dutoit <louis.dutoitld@gmail.com>
2021-01-18 16:28:59 +0100

    :wrench:

--- conduit/settings.py
+++ conduit/settings.py
@@ -25,8 +25,8 @@ class Config(object):
         'http://localhost:4200',
         'http://0.0.0.0:4000',
         'http://localhost:4000',
-        'http://0.0.0.0:8080',
-        'https://0.0.0.0:8080',
+        'http://0.0.0.0:' + os.environ.get('PORT', '8080'),
+        'https://0.0.0.0:' + os.environ.get('PORT', '8080'),
         'https://devops-final-front.netlify.app'
     ]
     JWT_HEADER_TYPE = 'Token'
